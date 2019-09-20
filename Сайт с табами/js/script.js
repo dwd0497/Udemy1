eeecf7e71a92591020d8cf0f597431b62554b99c
dwd <dwd0497@gmail.com>
2019-09-20 11:27:38 +0500

    added modal window

--- Сайт с табами/js/script.js
+++ Сайт с табами/js/script.js
@@ -67,7 +67,7 @@ window.addEventListener('DOMContentLoaded', function() {
                         if(num <= 9) {
                             return '0' + num;
                         } else return num;
-                    };
+                  };
 
             hours.textContent = addZero(t.hours);
             minutes.textContent = addZero(t.minutes);
@@ -84,4 +84,33 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+
+    // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
 });
+
+
+
+let age = document.getElementById('age');
+ 
+ function showUser(surname, name) {
+    alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+ }
+ 
+ showUser.apply(age, ["Горький","Максим"]);
